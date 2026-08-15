f171e79009d79a116048ff4e388d1be5f1839cba
Jack Leahey <jackleahey.42@gmail.com>
2026-08-15 11:59:31 -0400

    feat: connect dev environment to networking module

---
+++ environments/dev/main.tf
@@ -0,0 +1,26 @@
+#Calls the reusable networking module for the development environment
+module "networking" {
+  #Tells Terraform where the networking module is located relative to the dev directory
+  source = "../../modules/networking"
+
+  # Passes the project name from the dev environment into the networking module.
+  project_name = var.project_name
+
+  # Passes the environment name into the networking module.
+  environment = var.environment
+
+  #Passes the VPC CIDR block into the networking module
+  vpc_cidr = var.vpc_cidr
+
+  #Passes the list of public subnet CIDR blocks into the networking module
+  public_subnet_cidrs = var.public_subnet_cidrs
+
+  #Passes the lis of private subnet CIDR blocks into the networking module
+  private_subnet_cidrs = var.private_subnet_cidrs
+
+  #Passes the list of Availability Zones into the networking module
+  availability_zones = var.availability_zones
+
+  #Passes the NAT Gateway enable/disable setting into the networking module
+  enable_nat_gateway = var.enable_nat_gateway
+}
